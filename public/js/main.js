7622c7fcf51732007a0f273cf628749382f849c7
Erick Santos <erick.sousa@hotmail.com.br>
2022-12-18 20:57:39 -0300

    💄 mari page rework, added slideshow

--- public/js/main.js
+++ public/js/main.js
@@ -8,3 +8,24 @@ document.querySelectorAll('.nav-link').forEach(link => {
 // music volume adjust
 let audio = document.getElementById("bg"); 
 audio.volume = 0.06;
+
+
+// slideshow
+const slideshow = document.querySelector('#slide');
+const image = slideshow.querySelectorAll('img');
+const imgCount = slideshow.querySelector('#count')
+let counter = 0;
+
+// start setup
+imgCount.innerHTML = `${counter + 1} of ${image.length}`
+image[counter].style.display = `block`;
+
+// clickable arrows
+function changeImage(indicator) {
+    image[counter].style.display = `none`;
+    counter = (counter + (indicator > 0 ? indicator : indicator + image.length)) % image.length; // wtf?
+    // console.log(counter);
+    image[counter].style.display = `block`;
+
+    imgCount.innerHTML = `${counter + 1} of ${image.length}`
+}
