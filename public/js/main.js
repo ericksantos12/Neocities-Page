c8447d2ff3af01e978a68c0feea2ed9f01e3db42
Erick Santos <16109127+ericksantos12@users.noreply.github.com>
2023-05-14 19:36:36 -0300

    🔥 removed nav auto highlight
    neocities route issues :(

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,3 @@
-// auto nav link highlighter
-document.querySelectorAll('.nav-link').forEach(link => {
-    if (link.href === window.location.href) {
-        link.setAttribute('aria-current', 'page')
-    }
-});
-
 // music volume adjust
 let audio = document.getElementById("bg"); 
 audio.volume = 0.06;
