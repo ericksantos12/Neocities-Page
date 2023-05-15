86f42cd3b73d8edc5977b6085a147986d1abb12d
Erick Santos <16109127+ericksantos12@users.noreply.github.com>
2023-05-15 10:32:35 -0300

    🥚 added an easter egg

--- public/js/main.js
+++ public/js/main.js
@@ -1,55 +1,76 @@
 // music volume adjust
-let audio = document.getElementById("bg"); 
+let audio = document.getElementById("bg");
 audio.volume = 0.06;
 
-// calculate age
-const ageElement = document.querySelector('#age');
-let age = getAge('2003-04-08')
-ageElement.innerHTML = age;
+let easterFound = sessionStorage.getItem("easterFound");
+
+if (easterFound) createEasterItem();
 
 
-// slideshow
-const slideshow = document.querySelector('#slide');
-const image = slideshow.querySelectorAll('img');
-const imgCount = slideshow.querySelector('#count')
-let counter = 0;
-
-// start setup
-imgCount.innerHTML = `${counter + 1} of ${image.length}`
-image[counter].style.display = `block`;
-
-
-function changeImage(indicator) {
+function easter() {
     /**
-     * Changes the displayed image based on the indicator value.
-     *
-     * @param {number} indicator - Determines which direction to change the image.
-     *                             Positive numbers move forward, negative numbers move backward.
-     * @return {void}
+     * This function checks if the elements with IDs `alpha`, `beta`, and `gamma`
+     * exist on the page and sets a `sessionStorage` item for each element found.
+     * Then it checks if all three elements have been found and if so, plays an audio
+     * file, sets another `sessionStorage` item, displays an alert, and reloads the page.
      */
 
-    image[counter].style.display = `none`;
-    counter = (counter + (indicator > 0 ? indicator : indicator + image.length)) % image.length; // wtf?
-    image[counter].style.display = `block`;
+    alpha = document.querySelector('#alpha');
+    beta = document.querySelector('#beta');
+    gamma = document.querySelector('#gamma');
 
-    imgCount.innerHTML = `${counter + 1} of ${image.length}`
-}
-
-function getAge(dateString) {
-    /**
-     * Calculate the age based on a given birth date.
-     * https://stackoverflow.com/a/7091965
-     * 
-     * @param {string} dateString - A string representing the birth date in the format yyyy-mm-dd.
-     * @returns {number} - The age calculated from the birth date.
-     */
-
-    var today = new Date();
-    var birthDate = new Date(dateString);
-    var age = today.getFullYear() - birthDate.getFullYear();
-    var m = today.getMonth() - birthDate.getMonth();
-    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-        age--;
+    if (alpha) {
+        console.log(`found alpha`);
+        sessionStorage.setItem("alpha", true);
     }
-    return age;
+    if (beta) {
+        console.log(`found beta`);
+        sessionStorage.setItem("beta", true);
+    }
+    if (gamma) {
+        console.log(`found gamma`);
+        sessionStorage.setItem("gamma", true);
+    }
+
+    const eggs = [sessionStorage.getItem("alpha"), sessionStorage.getItem("beta"), sessionStorage.getItem("gamma")];
+
+    if (eggs.every(Boolean)) {
+        const foundAudio = new Audio('assets/audio/chest_opening.mp3')
+        foundAudio.play();
+        sessionStorage.setItem("easterFound", true);
+
+        alert("Você encontrou!")
+        location.reload();
+    }
 }
+
+function createEasterItem() {
+    /**
+     * Creates an Easter Egg item in the navbar.
+     */
+
+    const navbarNav = document.querySelector('.navbar-nav');
+
+    const navItem = document.createElement('li');
+    const navLink = document.createElement('a');
+    const img = document.createElement('img');
+    const span = document.createElement('span');
+    const title = document.createTextNode('Amandita');
+
+    navItem.classList.add('nav-item');
+    navLink.classList.add('nav-link');
+    span.classList.add('link-text');
+
+    navLink.setAttribute('href', 'amandita.html');
+    img.setAttribute('src', 'assets/icons/amandita.png');
+
+    span.appendChild(title);
+    navLink.appendChild(img);
+    navLink.appendChild(span);
+    navItem.appendChild(navLink);
+
+    navbarNav.appendChild(navItem);
+}
+
+
+
